6a4839c4315609755981e0b3351e50ebc9cc1e41
linustalacko <83816641+linustalacko@users.noreply.github.com>
2022-08-16 17:29:23 +1000

    landing page done

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.forms import CharField, PasswordInput
 
-
+class Login(models.Model):
+    username = CharField(max_length=42, min_length=42)
+    password = PasswordInput()
---
+++ static/js/index.js
@@ -0,0 +1,2 @@
+console.log('fuck')
+console.log(0.35+0.38+0.75)
